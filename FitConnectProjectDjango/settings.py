7722791d56950b20d4ff1496d7b133cf67205cf1
RonSamuel <109104664+RonSamuel@users.noreply.github.com>
2023-10-22 01:21:09 -0400

    Change DB connection

--- FitConnectProjectDjango/settings.py
+++ FitConnectProjectDjango/settings.py
@@ -75,10 +75,15 @@ WSGI_APPLICATION = "FitConnectProjectDjango.wsgi.application"
 # Database
 # https://docs.djangoproject.com/en/4.2/ref/settings/#databases
 
+# change to your database connection
 DATABASES = {
-    "default": {
-        "ENGINE": "django.db.backends.sqlite3",
-        "NAME": BASE_DIR / "db.sqlite3",
+    'default': {
+        'ENGINE': 'django.db.backends.mysql',
+        'NAME': 'fitness',
+        'USER': 'root',
+        'PASSWORD': '1234',
+        'HOST': 'localhost',
+        'PORT': '3306',
     }
 }
 
